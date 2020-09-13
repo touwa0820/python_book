2e493317f6d49855ae7f73c2a70d7301fa99413b
“touwa0820” <touwa100@gmail.com>
2020-09-13 20:51:25 +0900

    list_for-enumerate

---
+++ list_for-enumerate.py
@@ -0,0 +1,3 @@
+names = ["鈴木","田中","栗林","山岡"]
+for i,who in enumerate(names,1) :
+    print(f"{i}:{who}さん")
---
+++ list_for.py
@@ -0,0 +1,7 @@
+numbers = [2,6,-3,5,-1,7]
+sum = 0
+
+for num in numbers:
+    if num >0 :
+        sum += num
+print(sum)
